8501938187e07a42866eb17e5ce576745a8ed5de
hrsh7th <629908+hrsh7th@users.noreply.github.com>
2023-05-08 16:06:33 +0900

    Support multibyte

--- lua/linkedit/source/lsp.lua
+++ lua/linkedit/source/lsp.lua
@@ -1,6 +1,7 @@
 local kit = require('linkedit.kit')
 local Async = require('linkedit.kit.Async')
 local Client = require('linkedit.kit.LSP.Client')
+local Range = require('linkedit.kit.LSP.Range')
 
 ---@type linkedit.Source
 local Source = {}
@@ -26,11 +27,17 @@ function Source:fetch(params)
       return Async.resolve(nil)
     end
 
+    ---@type linkedit.kit.LSP.TextDocumentLinkedEditingRangeResponse?
     local response = client:textDocument_linkedEditingRange(params):await()
     if not response then
       return Async.resolve(nil)
     end
 
+    local bufnr = vim.uri_to_bufnr(params.textDocument.uri)
+    for i in ipairs(response.ranges) do
+      local lines = vim.api.nvim_buf_get_lines(bufnr, response.ranges[i].start.line, response.ranges[i]['end'].line + 1, false)
+      response.ranges[i] = Range.to_utf8(lines[1], lines[#lines], response.ranges[i], client.client.offset_encoding)
+    end
     return Async.resolve(response)
   end)
 end
